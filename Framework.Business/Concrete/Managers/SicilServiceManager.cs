1d5059da80bd3ed218c825afdb296eb9b5d1cf3a
SerhatOz <SerhatOz@DESKTOP-2C9MLGV>
2020-04-29 16:32:05 +0300

    Sicil Servis eklendi.

---
+++ Framework.Business/Concrete/Managers/SicilServiceManager.cs
@@ -0,0 +1,37 @@
+﻿using Framework.Business.Abstract;
+using Framework.Entities.ComplexTypes;
+using Framework.WebServis.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Business.Concrete.Managers
+{
+    public class SicilServiceManager : ISicilService
+    {
+        public SicilServisVm GetirSicilBilgisi(string VergiNo, string TcKimlikNo)
+        {
+            WebServis.Clients.SicilServiceClient serviceClient = new SicilServiceClient();
+
+            var result = serviceClient.GetirSicilBilgisi(VergiNo, TcKimlikNo);
+
+            SicilServisVm sicilBilgi = new SicilServisVm()
+            {
+                SicilNo = result.sicilNo,
+                VergiNo = result.vergiNo,
+                VergiDairesi = result.vergiDairesi,
+                Ad = result.adi,
+                Soyad = result.soyadi,
+                Il=result.isIlAdi,
+                Ilce=result.isIlceAdi,
+                Mahalle=result.isMahalleAdi,
+                Sokak=result.isSokakAdi,
+                AcikAdres=result.isAdres
+            };
+
+            return sicilBilgi;
+        }
+    }
+}
---
+++ Framework.Entities/ComplexTypes/SicilServisVm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Framework.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Framework.Entities.ComplexTypes
+{
+    public class SicilServisVm
+    {
+        public string SicilNo { get; set; }
+
+        public string VergiNo { get; set; }
+
+        public string TcKimlikNo { get; set; }
+
+        [StringLength(500)]
+        public string Ad { get; set; }
+
+        [StringLength(500)]
+        public string Soyad { get; set; }
+
+        [StringLength(500)]
+        public string Tanim { get; set; }
+
+        public string Il{ get; set; }
+
+        public string Ilce { get; set; }
+
+        public string Mahalle { get; set; }
+
+        public string Sokak { get; set; }
+
+        [StringLength(500)]
+        public string VergiDairesi { get; set; }
+
+        [StringLength(1500)]
+        public string AcikAdres { get; set; }
+    }
+}
