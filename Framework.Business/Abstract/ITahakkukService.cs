5cb218d04d3694815533cafba25573d477136c4b
SerhatOz <SerhatOz@DESKTOP-2C9MLGV>
2020-05-06 15:54:22 +0300

    Tahakkuk servisi düzenlendi.

---
+++ Framework.Business/Abstract/ITahakkukService.cs
@@ -0,0 +1,11 @@
+﻿using Framework.Entities.ComplexTypes;
+
+namespace Framework.Business.Abstract
+{
+    public interface ITahakkukService
+    {
+        TahakkukSorguSonucVm TahakkukSorgulama(string BorcId);
+
+        TahakkukEkleSonucVm TahakkukOlustur(TahakkukEkleVm tahakkukBilgi);
+    }
+}
